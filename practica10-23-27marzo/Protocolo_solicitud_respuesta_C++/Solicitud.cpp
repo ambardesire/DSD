5e052df0886d4f8867321a69ff184228ac30346b
ambardesire <desif19@gmail.com>
2020-03-27 14:41:58 -0600

    Solicitud - Respuesta

---
+++ practica10-23-27marzo/Protocolo_solicitud_respuesta_C++/Solicitud.cpp
@@ -0,0 +1,42 @@
+#include "SocketDatagrama.h"
+#include "Solicitud.h"
+#include "mensaje.h"
+
+Solicitud::Solicitud(struct timeval timeout) {
+	timeoutSocket = timeout;
+	socketlocal = new SocketDatagrama(0,timeout);
+}
+
+char * Solicitud::doOperation(char* IP, int puerto, int operationId, char* arguments) {
+	struct mensaje msj;
+	char* resultado;
+	int res;
+	for(int i=0;i<10000;i++){	
+		msj.messageType = 0;
+		msj.requestId = id;
+		id++;
+		memcpy(msj.IP, IP, 16);
+		msj.puerto = puerto;
+		msj.operationId = operationId;	
+		//cout << "Id operacion: " << msj.operationId << endl;
+		//cout << "ip: " << msj.IP << endl;
+		memcpy(msj.arguments, arguments, 100);
+		//cout << "puerto: " << msj.puerto << endl;
+		//cout << "argumentos: " << msj.arguments << endl;
+		PaqueteDatagrama paq((char*) &msj, sizeof(msj), IP, puerto);
+		socketlocal->envia(paq);
+		PaqueteDatagrama paq1(sizeof(msj));
+		res = socketlocal->recibeTimeout(paq1,timeoutSocket.tv_sec,timeoutSocket.tv_usec);
+		cout<<"Resultado: " <<res <<endl;
+		/*if(res>=0){
+			resultado = paq1.obtieneDatos();
+			break;
+		}*/
+	}	
+	if(res>=0)
+		cout << "resultado: " << resultado << endl;
+	else
+		cout << "No se pudo conectar al servidor :(" << endl;
+
+	return resultado;
+}
