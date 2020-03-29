b1833ad1c99393e4f05ea03242156e6912b12b5e
jmanjma <45376226+jmanjma@users.noreply.github.com>
2020-03-29 13:15:03 -0600

    Add files via upload

---
+++ practica10-23-27marzo/timeout/SocketDatagrama.h
@@ -0,0 +1,34 @@
+#ifndef __SocketDatagrama__
+#define __SocketDatagrama__
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <strings.h>
+#include "PaqueteDatagrama.h"
+#include <arpa/inet.h>
+
+#include <fstream>
+#include <errno.h>
+#include <sys/time.h>
+
+class SocketDatagrama{
+
+public:
+    SocketDatagrama(int puerto);
+    int recibe(PaqueteDatagrama & p);
+    SocketDatagrama(int puerto, struct timeval);
+    int recibeTimeout(PaqueteDatagrama & p, time_t segundos, suseconds_t microsegundos);
+    ~SocketDatagrama();
+    int envia(PaqueteDatagrama & p);
+private:
+    struct sockaddr_in direccionLocal;
+    struct sockaddr_in direccionForanea;
+    int s;
+	struct timeval timeoutSocket;
+};
+
+#endif
