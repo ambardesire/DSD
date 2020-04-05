6e4297e93d0a26044cd4c1d8f72a10be217e6a89
jmanjma <45376226+jmanjma@users.noreply.github.com>
2020-04-04 18:47:43 -0600

    Add files via upload

--- practica11-30mar-3abr/Ejercicio_2/cliente.cpp
+++ practica11-30mar-3abr/Ejercicio_2/cliente.cpp
@@ -4,27 +4,21 @@ using namespace std;
 
 #include "Solicitud.h"
 #include "SocketDatagrama.h"
-#include <time.h>           // time
-#include <string.h>         // bzero
 
 int main(int argc, char *argv[]) {
     Solicitud sol = Solicitud();
-    srand(time(NULL));
 
     char *ip = argv[1];
     int puerto = atoi(argv[2]);
-    int n = atoi(argv[3]);
-    char *pesos = new char[1];
-    int ndb = 0;
+    int n = atoi(argv[5]);
 
-    char argumentos[4000];
+    char argumentos[4000] = "";
+    strcat(argumentos, argv[3]);
+    strcat(argumentos, " ");
+    strcat(argumentos, argv[4]);
 
     for (int i=0 ; i<n ; i++) {
-        bzero(argumentos, 4000);
-        pesos[0] = (char)(rand()%9 + 49);
-        strcat(argumentos, pesos);
-        printf("%s\n", argumentos);
-        sol.doOperation(ip, puerto, 1, argumentos);
+        sol.doOperation(ip, puerto, i+1, argumentos);
     }
 
     return 0;
